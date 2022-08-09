bcd169848d0955017015ab4d0fae6898de969f5c
Tulsi <tulsiii1104@gmail.com>
2022-08-09 08:28:34 +0530

    finished

---
+++ src/main/kotlin/practical1_1.kt
@@ -0,0 +1,22 @@
+fun main(){
+    var a:Int=10
+    var b:Float=1.2f
+    var c:Char='T'
+    var String="Tulsi"
+    var Boolean=true
+    var Double=11.6
+    var Long=982762343452
+    var short:Short=-4
+    var Byte:Byte=127
+
+    println("Integer value:$a")
+    println("Float value:$b")
+    println("Character value:$c")
+    println("String value:$String")
+    println("Boolean value:$Boolean")
+    println("Double value:$Double")
+    println("Long value:$Long")
+    println("Short value:$short")
+    println("Byte value:$Byte")
+
+}
---
+++ src/main/kotlin/practical1_2.kt
@@ -0,0 +1,20 @@
+fun main(){
+    //Int to double
+    var a:Int=10
+
+    var b:Double=a.toDouble()
+    println("Double value (from Integer):$b")
+
+    //str to int
+    var c:String="4456"
+
+    var d:Int=c.toInt()
+    println("Int value (from string):$d")
+
+    //str to double
+    var e:String="555"
+
+    var f:Double=e.toDouble()
+    println("Double value (from string):$f")
+
+}
